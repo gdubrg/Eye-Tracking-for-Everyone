2108c758ef9e9c84a01cb37cf64581de810c179d
Guido <makuto@hotmail.it>
2017-11-06 18:43:30 -0800

    Add test for the complete dataset

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
 import argparse
 from train import train
+from test_small import test_small
+from test_big import test_big
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-train', action='store_true', help='train flag')
+    parser.add_argument('-test', action='store_true', help='train flag')
     parser.add_argument('-data', type=str, default='small', help='which dataset, small or big')
     parser.add_argument('-max_epoch', '--max_epoch', type=int, default=100, help='max number of iterations (default 100)')
     parser.add_argument('-batch_size', type=int, default=128, help='batch size (default 50)')
     parser.add_argument('-patience', type=int, default=15, help='early stopping patience (default 10)')
-    parser.add_argument('-dev', type=str, default=-1, help='what cpu or gpu (recommended) use to train the model')
+    parser.add_argument('-dev', type=str, default="-1", help='what cpu or gpu (recommended) use to train the model')
     args = parser.parse_args()
 
+    # train
     if args.train:
         train(args)
+
+    # test on small dataset
+    if args.test and args.data == "small":
+        test_small(args)
+
+    # test on original dataset
+    if args.test and args.data == "big":
+        test_big(args)
+
---
+++ test_big.py
@@ -0,0 +1,87 @@
+import os
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"] = "4"
+
+from load_data import load_data_names, load_batch_from_names, load_batch_from_names_fixed
+from models import get_eye_tracker_model
+import numpy as np
+
+
+def generator(data, batch_size, img_ch, img_cols, img_rows):
+
+    while True:
+        for it in list(range(0, data[0].shape[0], batch_size)):
+            x, y = load_batch([l[it:it + batch_size] for l in data], img_ch, img_cols, img_rows)
+            yield x, y
+
+
+def test_big(args):
+
+    names_path = ""
+    print("Names to test: {}".format(names_path))
+
+    dataset_path = ""
+    print("Dataset: {}".format(names_path))
+
+    weights_path = ""
+    print("Weights: {}".format(weights_path))
+
+    # image parameter
+    img_cols = 64
+    img_rows = 64
+    img_ch = 3
+
+    # test parameter
+    batch_size = 64
+    chunk_size = 500
+
+    # model
+    model = get_eye_tracker_model(img_ch, img_cols, img_rows)
+
+    # model summary
+    model.summary()
+
+    # weights
+    print("Loading weights...")
+    model.load_weights(weights_path)
+
+    # data
+    test_names = load_data_names(names_path)
+
+    # limit amount of testing data
+    # test_names = test_names[:1000]
+
+    # results
+    err_x = []
+    err_y = []
+
+    print("Loading testing data...")
+    for it in list(range(0, len(test_names), chunk_size)):
+
+        x, y = load_batch_from_names_fixed(test_names[it:it + chunk_size],  dataset_path, img_ch, img_cols, img_rows)
+        # x, y = load_batch_from_names(test_names[it:it + chunk_size], dataset_path, img_ch, img_cols, img_rows)
+        predictions = model.predict(x=x, batch_size=batch_size, verbose=1)
+
+        # print and analyze predictions
+        for i, prediction in enumerate(predictions):
+            print("PR: {} {}".format(prediction[0], prediction[1]))
+            print("GT: {} {} \n".format(y[i][0], y[i][1]))
+
+            err_x.append(abs(prediction[0] - y[i][0]))
+            err_y.append(abs(prediction[1] - y[i][1]))
+
+    # mean absolute error
+    mae_x = np.mean(err_x)
+    mae_y = np.mean(err_y)
+
+    # standard deviation
+    std_x = np.std(err_x)
+    std_y = np.std(err_y)
+
+    # final results
+    print("MAE: {} {} ( samples)".format(mae_x, mae_y))
+    print("STD: {} {} ( samples)".format(std_x, std_y))
+
+
+if __name__ == '__main__':
+    test_big()
